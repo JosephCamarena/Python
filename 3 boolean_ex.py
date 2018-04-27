a3801688cde0b97d5abd95d475b161db026ccc69
Joseph Camarena <joseph.a.camarena@gmail.com>
2018-04-26 19:23:34 -0700

    Update and rename boolean_ex.py to 3 boolean_ex.py

---
+++ 3 boolean_ex.py
@@ -0,0 +1,8 @@
+#Example using Boolean expressions
+name = "First"
+if name == "First":
+	print("Sweet")
+elif name == "What":
+	print("Heh")
+else:
+	print("Carry on")
--- boolean_ex.py
+++
@@ -1,7 +0,0 @@
-name = "First"
-if name == "First":
-	print("Yee")
-elif name == "Wut":
-	print("Heh")
-else:
-	print("Carry on")
