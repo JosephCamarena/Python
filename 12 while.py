a46dfb4bb981fb4a1133ceaf7660cf82f861324b
Joseph Camarena <joseph.a.camarena@gmail.com>
2018-04-26 19:41:58 -0700

    Update and rename while.py to 12 while.py

--- 12 while.py
+++ 12 while.py
@@ -1,24 +1,26 @@
-# msg = input("What's the secret password? ")
-# while msg != "bananas":
-# 	print("WRONG!")
-# 	msg = input("What's the secret password? ")
-# print("CORRECT!")
+# while loops
+msg = input("What's the secret password? ")
+while msg != "bananas":
+	print("WRONG!")
+	msg = input("What's the secret password? ")
+print("CORRECT!")
 
 # for num in range(1,11):
 # 	print(num)
-
 # num = 1
 # while num < 11:
 # 	print(num)
 # 	num += 1
 
 # cannot be duplicated in a For Loop
+# this code represents a sibling copying everything you say
 msg = input("Hey how's it going? ")
 while msg != "stop copying me":
 	print(msg)
 	msg = input()
 print("UGH FINE YOU WIN")
 
+#another solution to above
 msg = input("Hey how's it going? ")
 while msg != "stop copying me":
 	msg = input(f"{msg}\n")
