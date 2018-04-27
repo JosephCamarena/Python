5c2f494480f81d818061b9d7b624cf022d442e1a
Joseph Camarena <joseph.a.camarena@gmail.com>
2018-04-26 19:24:34 -0700

    Update and rename logic.py to 4 logic.py

--- 4 logic.py
+++ 4 logic.py
@@ -1,14 +1,17 @@
 # showcasing we could have multiple elif's
 color = input("What's your favorite color? ")
+
 if color == 'purple':
 	print("excellent choice!")
+	
 elif color == 'teal':
 	print("not bad!")
+	
 elif color == 'seafoam':
 	print("mediocre")
+	
 elif color == 'pure darkness':
 	print("I like how you think")
+	
 else:
 	print("YOU MONSTER!")
-
-
