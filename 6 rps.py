3e87c3cbbd5e487ae47b68396aa86a7a84c2d504
Joseph Camarena <joseph.a.camarena@gmail.com>
2018-04-26 19:30:47 -0700

    Update and rename rps.py to 6 rps.py

--- 6 rps.py
+++ 6 rps.py
@@ -1,10 +1,13 @@
- # basic version; on  my own
+# basic version of Rock Paper Scissors
+##########################
+#####     PROMPT     #####
+##########################
 # ...rock...
 # ...paper...
 # ...scissors...
 # (enter player 1's choice): rock
 # (enter player 2's choice): paper
-# SHOOT!
+# print("SHOOT!")
 # player2 wins
 
 print("...rock...\n...paper...\n...scissors...")
@@ -12,22 +15,22 @@ player1 = input("Player 1, enter your choice: ")
 player2 = input("Player 2, enter your choice: ")
 print("SHOOT!")
 if player1 and player2:
+	# all possible combinations for player 1 winning
 	if (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
 		print("Player 1 wins!")
+	# all possibile combinations for player 2 winning
 	elif (player1 == "paper" and player2 == "scissors") or (player1 == "scissors" and player2 == "rock") or (player1 == "rock" and player2 == "paper"):
 		print("Player 2 wins!")
-	elif player1 == player2: #teacher code
-	# elif (player1 == "rock" and player2 == "rock") or (player1 == "paper" and player2 == "paper") or (player1 == "scissors" and player2 == "scissors"):
+	# Tie between both players
+	elif player1 == player2:
 		print("Tie!")
+	# error handling
 	else:
 		print("Woops, something went wrong!")		
 else:
 	print("Type rock, paper, or scissors!")
 
-	# elif (player1 == "rock" and player2 == "rock") or (player1 == "paper" and player2 == "paper") or (player1 == "scissors" and player2 == "scissors"):
-	# 	print("Tie!")
-
-# instructor code
+# sample code
 # if player1 == player2:
 # 	print("Its a tie!")
 # elif player1 == "rock":
